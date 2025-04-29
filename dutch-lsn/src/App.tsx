ee851e5446fbc4f6d3128edff2f88f685cde21d5
Kateryna Klimchuk <ketyclima@gmail.com>
2025-04-29 10:17:09 +0200

    Add a column, implement gh-pages deploy

--- dutch-lsn/src/App.tsx
+++ dutch-lsn/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import { EditIcon } from "./Components/Icon/EditIcon";
 import { EyeIcon } from "./Components/Icon/EyeIcon";
 import { DeleteIcon } from "./Components/Icon/DeleteIcon";
@@ -12,26 +11,31 @@ function App() {
       location: "Bladel",
       teacher: "Mary N",
       level: "A0",
+      notes: "Any additional information",
     },
     {
       dayAndTime: "Tuesday 14:30",
       location: "Eersel",
       teacher: "Rob van der Meer",
       level: "A1",
+      notes: "",
     },
     {
       dayAndTime: "Thursday 9:00",
       location: "Vessem",
       teacher: "Mieke Vlass",
       level: "A0",
+      notes: "",
     },
     {
       dayAndTime: "Friday 9:00",
       location: "Eersel",
       teacher: "Jacob Roberts",
       level: "B1",
+      notes: "",
     },
   ];
+
   return (
     <div className="mx-auto">
       <header className="w-full p-4 bg-white shadow-md">
@@ -71,21 +75,24 @@ function App() {
                 <th className="p-4 border border-gray-400 text-left">
                   Student level
                 </th>
+                <th className="p-4 border border-gray-400 text-left">Notes</th>
               </tr>
             </thead>
             <tbody>
-              {data.map((user, index) => (
+              {data.map((item, index) => (
                 <tr key={index}>
                   <td className="p-4 border border-gray-400">
-                    {user.dayAndTime}
+                    {item.dayAndTime}
                   </td>
                   <td className="p-4 border border-gray-400">
-                    {user.location}
+                    {item.location}
                   </td>
-                  <td className="p-4 border border-gray-400">{user.teacher}</td>
+                  <td className="p-4 border border-gray-400">{item.teacher}</td>
+                  <td className="p-4 border border-gray-400">{item.level}</td>
+
                   <td className="p-4 border border-gray-400">
                     <div className="flex items-center justify-between">
-                      <p>{user.level}</p>
+                      <p>{item.notes}</p>
                       <div className="flex items-center justify-center gap-2">
                         <button className="border border-gray-400 rounded hover:bg-gray-100 p-1 px-2 transition">
                           <EyeIcon />
